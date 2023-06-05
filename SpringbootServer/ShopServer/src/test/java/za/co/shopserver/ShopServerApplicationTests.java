e8a576046b68863d6ef2ea130c7cb101feee97d7
BongaDigiLink <bonga@digilink.africa>
2023-06-05 12:12:49 +0200

    project start.

---
+++ SpringbootServer/ShopServer/src/test/java/za/co/shopserver/ShopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.shopserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
