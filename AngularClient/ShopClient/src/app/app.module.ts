f860e1ae119ed543ab1b83f270604e91344a8727
BongaGougota0 <bonga_gougota@gmail.com>
2023-06-18 02:11:18 +0200

    project setup. demo files (images) not included. project gen. structure compiles.

--- AngularClient/ShopClient/src/app/app.module.ts
+++ AngularClient/ShopClient/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Layouts/home/home.component';
+import { FooterComponent } from './Layouts/footer/footer.component';
+import { HeaderComponent } from './Layouts/header/header.component';
+import { NavigationComponent } from './Layouts/navigation/navigation.component';
+import { DetailComponent } from './Layouts/detail/detail.component';
+import { ShopComponent } from './Layouts/shop/shop.component';
+import { CartComponent } from './Layouts/cart/cart.component';
+import { CheckoutComponent } from './Layouts/checkout/checkout.component';
+import { ContactComponent } from './Layouts/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationComponent,
+    DetailComponent,
+    ShopComponent,
+    CartComponent,
+    CheckoutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
